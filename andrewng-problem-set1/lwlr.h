258c2cf14da66ced44e444919e370652a1e9abfd
root <root@localhost.localdomain>
2013-03-14 11:05:28 +1200

    ok

--- andrewng-problem-set1/lwlr.h
+++ andrewng-problem-set1/lwlr.h
@@ -2,42 +2,41 @@
 #include <stdlib.h>
 #include <math.h>
 
-double norm(double theta[100],double oldtheta[100])
-{
-	double sum = 0.0;
-	for(int i=0;i<100;++i)
-	{
-		sum += fabs(theta[i]-oldtheta[i]);
-	}
-	return sum;
-}
 
-float weight(float x,float xi,float tao)
+double weight(double x,double xi,double tao)
 {
-	return expf( -1*(x-xi)*(x-xi)/(2*tao*tao) );	
+	double ret =  exp( -1*(x-xi)*(x-xi)/(2*tao*tao) );	
+	return ret;
 }
-float theta[2]={0.1,0.1};
+double theta[2]={0.1,0.1};
 
-float x_times_theta(float x[2],float theta[2])
+double x_times_theta(double x[2],double theta[2])
 {
 	return x[0]*theta[0] + x[1]*theta[1];
 }
 
-void Loss(float X,float x[100][2],float* y ,float* ret)
+float Loss(double X,double tao, double x[100][2],double* y ,double* ret)
 {
 	
+	double r_t[2] = {0.0,0.0};
+	double l= 0.0;
 	for(int i=0;i<99;++i)
 	{
-		float temp = x_times_theta(theta,x[i])-y[i] ;
-		float r = 0.5*(weight(X,x[i][1],0.8)* powf(temp,2.0));
-		ret[0] += r*x[i][0];
-		ret[1] += r*x[i][1];
+		double temp = x_times_theta(theta,x[i])-y[i] ;
+		double r = (weight(X,x[i][1],tao)* pow(temp,2.0));
+		if(r<0.0) printf("error\n");
+		r_t[0] += r*x[i][0];
+		r_t[1] += r*x[i][1];
+		l += r;
 	}
+	ret[0] += r_t[0];
+	ret[1] += r_t[1];
+	return l;
 }
 int progress(const char* x_dat,const char* y_dat)
 {
 	//	read x_dat into x[100][2] 0->x0 1->x1
-	float x[100][2];
+	double x[100][2];
 	{
 		FILE * fp;
 		char * line = NULL;
@@ -56,7 +55,7 @@ int progress(const char* x_dat,const char* y_dat)
 	}
 
 	//	read y_dat into y[100]
-	float y[100];
+	double y[100];
 	{		
 		FILE * fp;
 		char * line = NULL;
@@ -72,21 +71,19 @@ int progress(const char* x_dat,const char* y_dat)
 		if (line)
 			free(line);
 	}
-	float predict_x = 4.0;
-	float last_v = 0.0;
-	for(int i=0;i<40;++i)
+	double predict_x = 4.0;
+	double last_l = 10000000.0; //make a max loss
+	for(int i=0;i<100;++i)
 	{
-		float loss[2]={0.1,0.1};
-		Loss(predict_x,x,y,&loss[0]);
-		theta[0] = theta[0] + 0.001*loss[0];
-		theta[1] = theta[1] + 0.001*loss[1];
-		//printf("%f\t%f\n",loss[0],loss[1]);
-
-		float predict_y = theta[0]+predict_x*theta[1];
-		printf("after %dth iterate %f\n",i,theta[0]+predict_x*theta[1]);
-		if(fabs(last_v-predict_y)<0.05) break;
-		last_v = predict_y;
+		double loss[2]={0.01,0.01};
+		double tao = 0.8;   //use 0.1  0.3  0.8 2 and 10 try in this homework
+		double l = Loss(predict_x,tao,x,y,&loss[0]);
+		theta[0] = theta[0] + 0.0001*loss[0];
+		theta[1] = theta[1] + 0.0001*loss[1];
+		if( l >= last_l) break;  //no loss decline at all , just quit
+		double predict_y = theta[0]+predict_x*theta[1];
+		printf("loss:%f,predict_x:%f,tao:%f,after %dth iterate %f\n",l,predict_x,tao,i,theta[0]+predict_x*theta[1]);
+		last_l = l;
 	}
-	printf("final:%f\n",theta[0]+predict_x*theta[1]);	 
 	
 }
